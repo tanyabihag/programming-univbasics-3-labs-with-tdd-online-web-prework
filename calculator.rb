925ac8941d56e81853ff150f10b5d049404923ce
Tanya Bihag <tbihag@gmail.com>
2019-08-07 22:04:32 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,3 +5,8 @@ second_number = 4
 
 sum = first_number + second_number
 
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
